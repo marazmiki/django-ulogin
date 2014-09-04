df6fa0b2e5c73b2a3d7977bec56cce15fa5f6d2e
Michael Porokhovnichenko <marazmiki@gmail.com>
2014-09-04 21:31:23 +0700

    fix for 1.7

--- tests.py
+++ tests.py
@@ -10,19 +10,29 @@ import os
 sys.path.insert(0, os.path.abspath(os.path.dirname(__file__)))
 
 
-settings.configure(DEBUG=True,
-                   ROOT_URLCONF='django_ulogin.tests.urls',
-                   INSTALLED_APPS=('django.contrib.auth',
-                                   'django.contrib.contenttypes',
-                                   'django.contrib.sessions',
-                                   'django_ulogin',),
-                   DATABASES={'default': {'ENGINE': 'django.db.backends.sqlite3',
-                                          'NAME': ':MEMORY:'}
-                              })
+settings.configure(
+    DEBUG=True,
+    ROOT_URLCONF='django_ulogin.tests.urls',
+    INSTALLED_APPS=(
+        'django.contrib.auth',
+        'django.contrib.contenttypes',
+        'django.contrib.sessions',
+        'django_ulogin',
+    ),
+    DATABASES={
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': ':MEMORY:'
+        }
+    })
 
 
 def main():
     from django.test.utils import get_runner
+    import django
+
+    if hasattr(django, 'setup'):
+        django.setup()
 
     find_pattern = 'django_ulogin'
 
