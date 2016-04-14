615100f0a2d12cfa251811c3274f4b8e99cb9778
mcd-php <mcd-php@users.noreply.github.com>
2016-04-14 10:24:19 +0300

    PEP8: def, not lambda

--- django_ulogin/templatetags/ulogin_tags.py
+++ django_ulogin/templatetags/ulogin_tags.py
@@ -50,7 +50,10 @@ def ulogin_widget(context, name="default"):
             'SCHEME_NOT_FOUND': True,
             'NAME': name
         }
-    glue = lambda key: ','.join([p for p in scheme.get(key, getattr(s, key))])
+
+    def glue(key):
+        return ','.join([p for p in scheme.get(key, getattr(s, key))])
+
     rand = ''.join(random.choice(string.ascii_lowercase) for x in range(5))
 
     return {
